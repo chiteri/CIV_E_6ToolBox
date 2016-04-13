79531f5000d33b33b870d898d233ed8706953053
chiteri <martin.chiteri@gmail.com>
2016-04-13 12:03:49 -0600

    Include all files suitable for outputting data to Mathematica's format

---
+++ CIV E 606/WinterSimProject/FormatForMathematica.py
@@ -0,0 +1,16 @@
+import csv 
+
+with open('Pendulum simulation - Results.csv') as csvfile:
+    reader = csv.DictReader(csvfile) 
+    result = []
+    for row in reader: 
+        # result.append(float(row['Theta - radians (5 m, 20 degrees)']))
+        # result.append(float(row['Theta - degrees (5 m, 20 degrees)']))
+        # result.append(float(row['Theta - radians (5 m, 15 degrees)']))
+        # result.append(float(row['Theta - degrees (5 m, 15 degrees)']))
+        # result.append(float(row['Theta - radians (2.5 m, 20 degrees)']))
+        # result.append(float(row['Theta - degrees (2.5 m, 20 degrees)']))
+        # result.append(float(row['Theta - radians (2.5 m, 15 degrees)']))
+        result.append(float(row['Theta - degrees (2.5 m, 15 degrees)']))
+
+    print (result)
